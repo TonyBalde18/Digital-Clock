277ab4dd0e7b52ec7a77ba33ad1d09df861d8baa
TonyBalde18 <antonio.balde@ubi.pt>
2023-07-10 08:21:40 +0100

    add date display

--- sandbox.js
+++ sandbox.js
@@ -1,21 +1,28 @@
 const clock = document.querySelector('.clock');
 
 const tick = () => {
-
   const now = new Date();
   
   const h = now.getHours();
   const m = now.getMinutes();
   const s = now.getSeconds();
 
+  const dayOfWeek = now.toLocaleDateString('en-US', { weekday: 'long' });
+  const month = now.toLocaleDateString('en-US', { month: 'long' });
+  const day = now.getDate();
+
   const html = `
-    <span>${h}</span> :
-    <span>${m}</span> :
-    <span>${s}</span>
+    <div class="time">
+      <span>${h}</span> :
+      <span>${m}</span> :
+      <span>${s}</span>
+    </div>
+    <div class="date">
+      ${dayOfWeek}, ${month} ${day}
+    </div>
   `;
 
   clock.innerHTML = html;
-
 };
 
 setInterval(tick, 1000);
